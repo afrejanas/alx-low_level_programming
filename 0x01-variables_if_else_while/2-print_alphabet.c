ecc18cf5074f507336612d74ce5301153e62ee0a
afrejanas <afrejanas@gmail.com>
2023-03-17 08:03:17 -0700

    convert myChar[]to lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Discription: prints the lowercase letters of the alphabet
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char myChar[] = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+
+	int i = 0;
+
+	for (i = 0; i < = 25; i = i + 1)
+	{
+		puthar(myChar[i] + 32);
+	}
+	putchar('\n');
+	return (0);
+}
