f57ac355fef95ddb17d6cf464ee8177b53ddf1d7
afrejanas <afrejanas@gmail.com>
2023-03-27 17:28:20 -0700

    prints n elements of an array of integers, followed by a new line.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@ void _puts(char *str);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 int _putchar(char c);
 void print_rev(char *s);
 void reset_to_98(int *n);
