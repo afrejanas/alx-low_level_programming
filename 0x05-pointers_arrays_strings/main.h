9d138ab852bfddb337e9db58e3c71f46bc317107
afrejanas <afrejanas@gmail.com>
2023-03-27 17:11:21 -0700

    prints every other character of a string, starting with the first character, followed by a new line.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void rev_string(char *s);
+void puts2(char *str);
 int _putchar(char c);
 void print_rev(char *s);
 void reset_to_98(int *n);
