40525bbc12ed3da747da2d0131bb1d44fc6da1e4
afrejanas <afrejanas@gmail.com>
2023-04-05 09:05:11 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - entry point
+ * @s:  string inputed
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}
+
