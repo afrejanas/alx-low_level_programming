21e875f2616340e4ddd6bf52a6e5f28013087cf6
afrejanas <afrejanas@gmail.com>
2023-04-17 15:51:51 -0700

    fixed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - preints the name of the file
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	printf("%s \n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
