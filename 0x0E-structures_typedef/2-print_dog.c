e14f1bddd9c46e62c112a0716f102a3b7ba61401
afrejanas <afrejanas@gmail.com>
2023-04-17 17:44:26 -0700

    betty style fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include"dog.h"
-
+/**
+ * print_dog - Prints all the data of a dog
+ * @d: A dog structure
+ *
+ * Return: Nothing
+ */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
@@ -9,15 +14,15 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("name: %s\n", "(nil)");
 		else
-			printf("name: %s\n",d->name);
+			printf("name: %s\n", d->name);
 		if (d->age == 0)
 			printf("age: %s\n", "(nil)");
 		else
-			printf("age: %f\n",d->age);
+			printf("age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("owner: %s\n", "(nil)");
 		else
-			printf("owner: %s\n",d->owner);
+			printf("owner: %s\n", d->owner);
 	}
 
 }
