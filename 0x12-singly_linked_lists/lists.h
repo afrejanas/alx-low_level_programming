1c599807f697a1bb3f407fd80675c2e75c939c46
afrejanas <afrejanas@gmail.com>
2023-04-27 18:33:32 -0700

    betty style fixed for lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,10 @@
-#ifndef LISTES_H
-#define LISTE_H
+#ifndef LISTS_H
+#define LISTS_H
 #include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
 /**
- * struct node - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -13,7 +13,7 @@
  * for alx project
  */
 
-typedef struct list_s 
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
