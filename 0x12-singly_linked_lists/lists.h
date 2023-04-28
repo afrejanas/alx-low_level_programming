d598002ceabbaef341d266594c48fd902034e3fb
afrejanas <afrejanas@gmail.com>
2023-04-27 17:40:22 -0700

    lists.h  updated

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,15 +1,24 @@
 #ifndef LISTES_H
 #define LISTE_H
-
+#include<string>
 #include<stdio.h>
 #include<stdlib.h>
-
+/**
+ * struct node - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for alx project
+ */
 
 typedef struct Node 
 {
-    int data;
-    struct Node* next;
-}node;
+	char *str;
+	unsigned int len;
+	struct Node* next;
+} node;
 
 size_t print_list(const list_t *h);
 #endif
