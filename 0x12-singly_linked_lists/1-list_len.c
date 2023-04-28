af9f27f0849ad295f4294323e7fb1fa6e39d86f6
afrejanas <afrejanas@gmail.com>
2023-04-27 18:41:49 -0700

    1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include"lists.h"
+
+/**
+ * list_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t list_len(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
