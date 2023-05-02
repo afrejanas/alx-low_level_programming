6adb180ece369c90a784c5ac38ff5cc1058b444e
afrejanas <afrejanas@gmail.com>
2023-05-01 18:46:53 -0700

    while loop fixed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t c = 1;
+	size_t c = 0;
 
-	while (h->next)
+	while (h)
 	{
 		c++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	printf("%d\n", h->n);
 	return (c);
 }
