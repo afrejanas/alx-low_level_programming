432e909152dc0c4f794ca995f9e82c55674efdab
afrejanas <afrejanas@gmail.com>
2023-06-15 09:12:25 -0700

    betty style fixed

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,10 +1,18 @@
 #include "lists.h"
+/**
+ * dlistint_len - print all the elements of a dlistint_t list
+ * @h: pointer to the start of the linked list
+ *
+ * Return: number of nodes
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t n = 0;
+
 	if (h == NULL)
-		return(0);
-	while(h && ++n)
+		return (0);
+	while (h && ++n)
 	{
 		h = h->next;
 	}
