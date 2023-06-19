579e05b49429505647ff84b16b407692d66782fb
afrejanas <afrejanas@gmail.com>
2023-06-19 07:48:50 -0700

    task 0

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _isdigit - checks for digits
+ * @c: character.
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
